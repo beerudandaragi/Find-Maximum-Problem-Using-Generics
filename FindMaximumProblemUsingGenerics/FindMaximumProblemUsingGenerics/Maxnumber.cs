31bb408f2e07a6e3ef34a683a1a2f5418da4cffb
beerappa <beerappadandaragi@gmail.com>
2021-11-18 10:00:23 +0530

    find maximum problem using generics

---
+++ FindMaximumProblemUsingGenerics/FindMaximumProblemUsingGenerics/Maxnumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxinumOfMoreThanThreeNumbers
+{
+    public class MaxNumbers
+    {
+        public static void maxNumbers()
+        {
+            int num1 = 554;
+            int num2 = 222;
+            int num3 = 822;
+            int num4 = 220;
+
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 && num1.CompareTo(num4) > 0 ||
+               num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 && num1.CompareTo(num4) > 0 ||
+                num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0 && num1.CompareTo(num4) > 0 ||
+                num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 && num1.CompareTo(num4) >= 0)
+
+            {
+                Console.WriteLine("num 1 is greater " + num1);
+            }
+
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 && num2.CompareTo(num4) > 0 ||
+               num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 && num2.CompareTo(num4) > 0 ||
+                num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0 && num2.CompareTo(num4) > 0 ||
+                num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 && num2.CompareTo(num4) >= 0)
+            {
+                Console.WriteLine("num 2 is greater " + num2);
+            }
+
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 && num3.CompareTo(num4) > 0 ||
+               num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 && num3.CompareTo(num4) > 0 ||
+                num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0 && num3.CompareTo(num4) > 0 ||
+                num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 && num3.CompareTo(num4) >= 0)
+            {
+                Console.WriteLine("num 3 is greater " + num3);
+            }
+
+            if (num4.CompareTo(num1) > 0 && num4.CompareTo(num2) > 0 && num4.CompareTo(num3) > 0 ||
+               num4.CompareTo(num1) >= 0 && num4.CompareTo(num2) > 0 && num4.CompareTo(num3) > 0 ||
+                num4.CompareTo(num1) > 0 && num4.CompareTo(num2) >= 0 && num4.CompareTo(num3) > 0 ||
+                num4.CompareTo(num1) > 0 && num4.CompareTo(num2) > 0 && num4.CompareTo(num3) >= 0)
+            {
+                Console.WriteLine("num 4 is greater " + num4);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- FindMaximumProblemUsingGenerics/FindMaximumProblemUsingGenerics/Program.cs
+++ FindMaximumProblemUsingGenerics/FindMaximumProblemUsingGenerics/Program.cs
@@ -1,23 +1,15 @@
-﻿using System;
-namespace CSharpProgram
-{
-    class GenericClass<T>
-    {
-        public GenericClass(T msg)
-        {
-            Console.WriteLine(msg);
-           // Console.ReadLine();
-        }
+﻿using MaxinumOfMoreThanThreeNumbers;
+using System;
 
-    }
+namespace MaximumOfMoreThanThreeNumbers
+{
     class Program
     {
         static void Main(string[] args)
         {
-            GenericClass<string> gen = new GenericClass<string>("This is generic class");
-            GenericClass<int> genI = new GenericClass<int>(101);
-            GenericClass<char> getCh = new GenericClass<char>('I');
-            Console.ReadLine();
+            MaxNumbers.maxNumbers();
+            Console.ReadKey();
+
         }
     }
 }
